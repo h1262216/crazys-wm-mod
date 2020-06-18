dc936911cf72062e49c080a6be281ad6585c9dcf
FueledByOCHD <fueledbyochd@gmail.com>
2020-06-18 12:03:14 -0600

    Updated Config removing redundant loading and redundant code

--- src/engine/include/Revision.h
+++ src/engine/include/Revision.h
@@ -1,8 +1,8 @@
 // game version
-const int g_MajorVersion    = 1;
-const int g_MinorVersionA   = 7;
+const int g_MajorVersion    = 2;
+const int g_MinorVersionA   = 0;
 const int g_MinorVersionB   = 0;
-const int g_StableVersion   = 1;
+const int g_StableVersion   = 0;
 
-static char svn_revision[] = "Crazy and PP's mod version .07.01.01";
+static char svn_revision[] = "Hellfire Version 1.0.0.0";
 
