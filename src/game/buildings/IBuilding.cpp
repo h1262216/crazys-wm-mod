2181c4e122303efe570605f74d714b52044674a7
=nada= <chrischan8719@gmail.com>
2021-08-19 11:43:31 +0200

    + Feature: Girl's stats display changes from last week

--- src/game/buildings/IBuilding.cpp
+++ src/game/buildings/IBuilding.cpp
@@ -105,7 +105,7 @@ void IBuilding::BeginWeek()
     std::vector<const sGirl*> dead_girls;
 
     std::stringstream ss;
-    m_Girls->apply([&](sGirl& cgirl){
+    m_Girls->apply([&](sGirl& cgirl) {
         m_Filthiness++;
 
         if (cgirl.is_dead())            // Remove any dead bodies from last week
@@ -121,6 +121,7 @@ void IBuilding::BeginWeek()
         }
         else
         {
+            cgirl.save_statistics();
             cgirl.m_Building = this;
             cgirl.m_Tort = false;
 
